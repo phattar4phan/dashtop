1cf39324d6636c79911c9b5ae82efc368fcf752e
phattar4phan <phattar4phan@gmail.com>
2026-07-28 18:21:23 +0700

    minor result return adjustment

--- src/utils/networking.py
+++ src/utils/networking.py
@@ -13,11 +13,9 @@ def GetNetworkingData(interval: float) -> dict:
     net_speed_recv = ((net_secnd_snapshot.bytes_recv - net_first_snapshot.bytes_recv) / (1024 ** 2)) / interval
     net_speed_sent = ((net_secnd_snapshot.bytes_sent - net_first_snapshot.bytes_sent) / (1024 ** 2)) / interval
     
-    return {
+    result = {
         'receive_speed': net_speed_recv,
         'sent_speed': net_speed_sent
     }
     
-networking = GetNetworkingData(1)
-print(f'Receive: {networking['receive_speed']:.2f} MB/s')
-print(f'Sent: {networking['sent_speed']:.2f} MB/s')
+    return result
--- src/utils/ram.py
+++ src/utils/ram.py
@@ -6,15 +6,10 @@ def GetRAMData() -> dict[str, float]:
     ram_used = ram.used / (1024 ** 3)
     ram_percent = ram.percent
     
-    return {
+    result = {
         'percent': ram_percent,
         'used': ram_used,
         'total': ram_total,
         'available': (ram_total - ram_used)
     }
-    
-# ram = GetRAMData()
-# print(f'RAM Usage: {ram['percent']:.2f}%')
-# print(f'RAM Used: {ram['used']:.2f} GB')
-# print(f'RAM Total: {ram['total']:.2f} GB')
-# print(f'Available: {ram['available']:.2f} GB')
+    return result
